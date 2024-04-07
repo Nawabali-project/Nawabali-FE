019feedc1b6db45a748fa982f5e04f0d20fdf5f9
hanaru <133945249+1roo@users.noreply.github.com>
2024-04-07 17:52:02 +0900

    [FIX] axios instance 수정

--- src/api/axios/axios.ts
+++ src/api/axios/axios.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { Cookies } from 'react-cookie';
+
+const cookie = new Cookies();
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL,
@@ -11,7 +14,7 @@ export const authInstance = axios.create({
 
 authInstance.interceptors.request.use(
   (config) => {
-    const accessToken = localStorage.getItem('access_Token');
+    const accessToken = cookie.get('accessToken');
     if (accessToken) {
       config.headers['Authorization'] = `${accessToken}`;
     } else {
@@ -23,43 +26,3 @@ authInstance.interceptors.request.use(
     return Promise.reject(error);
   },
 );
-
-authInstance.interceptors.response.use(
-  (res) => res,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        const newAccessToken = await refreshAccessToken();
-        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-        return authInstance(originalRequest);
-      } catch (refreshError) {
-        return Promise.reject(refreshError);
-      }
-    }
-    return Promise.reject(error);
-  },
-);
-
-const refreshAccessToken = async () => {
-  const refreshToken = window.localStorage.getItem('refresh_Token');
-
-  try {
-    const res = await authInstance.post(
-      `/refresh`,
-      {},
-      {
-        headers: {
-          Authorization: `${refreshToken}`,
-        },
-      },
-    );
-    window.localStorage.setItem('access_Token', res.data.accessToken);
-    window.localStorage.setItem('refresh_Token', res.data.refreshToken);
-    return res.data.accessToken;
-  } catch (err) {
-    console.error('엑세스 토큰 새로고침에 실패하였습니다', err);
-    throw err;
-  }
-};
