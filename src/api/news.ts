fc1aacd90a84325ab37b9cdbb8abecfe93bf5bf2
hanaru <133945249+1roo@users.noreply.github.com>
2024-04-11 18:42:41 +0900

    develop pull 커밋

--- src/api/news.ts
+++ src/api/news.ts
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
-import { instance } from '../../axios';
-=======
 import { instance } from '../axios';
->>>>>>> d440a807a13d5fdb8f34a91da1aa5043e03f7aba
-import { AxiosError } from 'axios';
-import { ErrorResponse } from 'react-router-dom';
 
-export const getPosts = async () => {
-  try {
-    const res = await instance.get('/posts');
-    return res;
-  } catch (error) {
-    throw error as AxiosError<ErrorResponse>;
-  }
+export const getAllPostsByDistrict = async (district: string) => {
+  const basicParams = '&page=0&size=10&sort=string';
+  const response = await instance.get(
+    `/posts/filtered?district=${district}${basicParams}`,
+  );
+  console.log(response);
+  return response.data.content;
 };
