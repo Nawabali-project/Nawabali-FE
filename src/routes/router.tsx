f509894564aa1a1d09a79b6e26aaf733c025f442
hanaru <133945249+1roo@users.noreply.github.com>
2024-04-16 05:12:26 +0900

    [FIX] 라우터 수정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -30,7 +30,7 @@ export const router = createBrowserRouter([
         element: <Myplace />,
       },
       {
-        path: '/mypage/edit',
+        path: '/edit',
         element: <EditUser />,
       },
       {
