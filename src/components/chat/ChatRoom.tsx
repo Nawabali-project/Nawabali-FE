f357d0d059db8f0cf15d40988791242dfe098029
hanaru <133945249+1roo@users.noreply.github.com>
2024-05-04 06:28:13 +0900

    [FIX] 채팅수정 제발

--- src/components/chat/ChatRoom.tsx
+++ src/components/chat/ChatRoom.tsx
@@ -162,7 +162,7 @@ export const ChatRoom: React.FC<{
               }
             />
             <MessageText isMyMessage={msg.sender === myNickname}>
-              {msg.message}
+              <Message>{msg.message}</Message>
               <MessageDate>
                 {formatMessageDate(msg.createdMessageAt)}
               </MessageDate>
@@ -228,9 +228,13 @@ const MessageRow = styled.div<MessageProps>`
   align-items: flex-end;
 `;
 
+const Message = styled.span`
+  width: calc(100%-30px);
+  word-wrap: break-word;
+`;
+
 const MessageText = styled.div<MessageProps>`
-  flex: 0 1 auto;
-  max-width: calc(100% - 30px);
+  width: 100%;
   display: flex;
   padding: 8px 12px;
   border-radius: 18px;
