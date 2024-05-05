d1bae19ccc86dafb0a9742e63d3ed6ccd0a936ef
inhachoi <80045891+inhachoi@users.noreply.github.com>
2024-05-05 20:48:29 +0900

    [REFACTOR] gif  원상복구

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -190,15 +190,20 @@ const MapPage = () => {
           </FourComponentBox>
         </SecondHeader>
       </CategoryBox>
-      {isLoading && <SkeletonMap />}
-      <CustomMap
-        width="100%"
-        height="811px"
-        clickedCategory={clickedKind}
-        selectedDistrict={selectedArea}
-        selectedLatitude={selectedLatitude}
-        selectedLongitude={selectedLongitude}
-      />
+      {isLoading ? (
+        <SkeletonMap />
+      ) : (
+        <>
+          <CustomMap
+            width="100%"
+            height="811px"
+            clickedCategory={clickedKind}
+            selectedDistrict={selectedArea}
+            selectedLatitude={selectedLatitude}
+            selectedLongitude={selectedLongitude}
+          />
+        </>
+      )}
     </Layout>
   );
 };
